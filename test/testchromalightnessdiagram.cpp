4aaccd1e286751318e71bfb1093c4b55766ca769
Lukas Sommer <sommerluk@gmail.com>
2021-05-11 06:54:29 +0000

    Add missing unit test file for ChromaLightnessDiagram

---
+++ test/testchromalightnessdiagram.cpp
@@ -0,0 +1,87 @@
+﻿// SPDX-License-Identifier: MIT
+/*
+ * Copyright (c) 2020 Lukas Sommer sommerluk@gmail.com
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#include "PerceptualColor/perceptualcolorglobal.h"
+#include "perceptualcolorinternal.h"
+
+// First included header is the public header of the class we are testing;
+// this forces the header to be self-contained.
+#include "chromalightnessdiagram.h"
+// Second, the private implementation.
+#include "chromalightnessdiagram_p.h"
+
+#include "PerceptualColor/rgbcolorspacefactory.h"
+
+#include <QtTest>
+
+namespace PerceptualColor
+{
+class TestChromaLightnessDiagram : public QObject
+{
+    Q_OBJECT
+
+public:
+    TestChromaLightnessDiagram(QObject *parent = nullptr)
+        : QObject(parent)
+    {
+    }
+
+private:
+    QSharedPointer<PerceptualColor::RgbColorSpace> m_rgbColorSpace = RgbColorSpaceFactory::create();
+
+private Q_SLOTS:
+    void initTestCase()
+    {
+        // Called before the first test function is executed
+    }
+
+    void cleanupTestCase()
+    {
+        // Called after the last test function was executed
+    }
+
+    void init()
+    {
+        // Called before each test function is executed
+    }
+
+    void cleanup()
+    {
+        // Called after every test function
+    }
+
+    void testConstructorDestructor()
+    {
+        ChromaLightnessDiagram test(m_rgbColorSpace);
+    }
+};
+
+} // namespace PerceptualColor
+
+QTEST_MAIN(PerceptualColor::TestChromaLightnessDiagram)
+
+// The following “include” is necessary because we do not use a header file:
+#include "testchromalightnessdiagram.moc"
